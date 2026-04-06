c12920936dc45c406a9d702959b0cf491edde436
lajsan <lajsanminlebaeva@mail.ru>
2026-04-06 12:58:05 -0700

    initial commit

---
+++ PetCare/PetActivity/PetActivityCreation/View/PetActivityCreationSection.swift
@@ -0,0 +1,12 @@
+//
+//  PetActivityCreationSection.swift
+//  PetCare
+//
+//  Created by Ляйсан on 5/4/26.
+//
+
+import Foundation
+
+enum PetActivityCreationSection: Int, CaseIterable {
+    case petPhotoSelection
+}
